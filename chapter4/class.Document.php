563252a7dd0602e46df6579626320f5d2a9cfde3
mawojast <inet.beck@gmail.com>
2023-02-07 23:46:09 +0100

    till page 109 - Error Handling

---
+++ chapter4/class.Document.php
@@ -0,0 +1,9 @@
+<?php
+class Document extends DomainObject {
+
+    public static function getGroup(): string {
+
+        return "document";
+    }
+}
+?>
--- chapter4/test.php
+++ chapter4/test.php
@@ -1,8 +1,13 @@
 <?php
 require_once(__DIR__."/require.php");
 
-$u = new UtilityService(100);
-echo $u->getFinalPrice();
+
+print_r(User::create());
+print_r(SpreadSheet::create());
+//$u = new UtilityService(100);
+//var_dump(User::create());
+
+//echo $u->getFinalPrice();
 //echo $u->PriceUtiltiesCalculateTax(100);
 
 //echo $u->calculateTax(100);
